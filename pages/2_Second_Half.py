51df163b8d95a75a2c76828c9ac17859b3202c38
zu56789 <zuhayrmohsen@gmail.com>
2024-10-01 17:22:10 +0100

    Added setup for second half page

--- pages/2_Second_Half.py
+++ pages/2_Second_Half.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="First Half")
+st.set_page_config(page_title="Second Half")
 
-st.title("First Half Data")
+st.title("Second Half Data")
 
 chels_tab, bayern_tab = st.tabs(["Chelsea", "Bayern Munich"])
