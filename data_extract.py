322cc4a33ff69f67d5b6b4b985169ce5fb3e9fd3
zu56789 <zuhayrmohsen@gmail.com>
2024-10-01 04:06:28 +0100

    Added extraction of shooting data

--- data_extract.py
+++ data_extract.py
@@ -13,10 +13,15 @@ def get_data(source): #method to get data from a url
 def get_passing_df(team):
     return events_df.loc[(events_df['type.name'] == 'Pass') & (events_df['possession_team.name'] == team)]
 
+def get_shooting_df(team):
+    return events_df.loc[(events_df['type.name'] == 'Shot') & (events_df['possession_team.name'] == team)]
+
 events_df = get_data("https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/events/18237.json")
 
 chels_passes_df = get_passing_df("Chelsea")
-
 bayern_passes_df = get_passing_df("Bayern Munich")
+
+chels_shots_df = get_shooting_df("Chelsea")
+bayern_shots_df = get_shooting_df("Bayern Munich")
 
 #print(events_df.head())
