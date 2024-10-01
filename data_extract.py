d9bb177ed33be0a87254a72b5b9dbd5aaba95cd9
zu56789 <zuhayrmohsen@gmail.com>
2024-10-01 04:05:04 +0100

    Added extraction of passing data

--- data_extract.py
+++ data_extract.py
@@ -9,6 +9,14 @@ def get_data(source): #method to get data from a url
     pd.set_option("display.max.columns", None)
     return df
 
+
+def get_passing_df(team):
+    return events_df.loc[(events_df['type.name'] == 'Pass') & (events_df['possession_team.name'] == team)]
+
 events_df = get_data("https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/events/18237.json")
 
-print(events_df.head())
+chels_passes_df = get_passing_df("Chelsea")
+
+bayern_passes_df = get_passing_df("Bayern Munich")
+
+#print(events_df.head())
