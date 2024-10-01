022d403c18de0e74b272f48de7492521f8d8b79f
zu56789 <zuhayrmohsen@gmail.com>
2024-10-01 18:26:37 +0100

    ET1 passing maps are now visible

--- pages/1_First_Half.py
+++ pages/1_First_Half.py
@@ -9,7 +9,9 @@ st.title("First Half Data")
 chels_tab, bayern_tab = st.tabs(["Chelsea", "Bayern Munich"])
 
 with chels_tab:
+    st.write("Passing map")
     st.pyplot(passing_pitch(chels_passes_df,1))
 
 with bayern_tab:
+    st.write("Passing map")
     st.pyplot(passing_pitch(bayern_passes_df,1))
--- pages/3_ET1.py
+++ pages/3_ET1.py
@@ -1,7 +1,17 @@
 import streamlit as st
+from data_extract import chels_passes_df, bayern_passes_df
+from draw_pitches import passing_pitch
 
 st.set_page_config(page_title="Extra Time 1")
 
 st.title("Extra time 1 data")
 
 chels_tab, bayern_tab = st.tabs(["Chelsea", "Bayern Munich"])
+
+with chels_tab:
+    st.write("Passing map")
+    st.pyplot(passing_pitch(chels_passes_df,3))
+
+with bayern_tab:
+    st.write("Passing map")
+    st.pyplot(passing_pitch(bayern_passes_df,3))
