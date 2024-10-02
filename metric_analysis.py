fd64bca63944a43b35503ba155b1f6a6a9223f5f
zu56789 <zuhayrmohsen@gmail.com>
2024-10-02 21:51:41 +0100

    Updated pass accuracy method to make half optional

--- metric_analysis.py
+++ metric_analysis.py
@@ -1,20 +1,35 @@
-def get_passing_accuracy(df, half):
+def get_passing_accuracy(df, half = 0):
 
     bad_passes = 0
+
     good_passes = 0
 
     for x in range(len(df['id'])):
-        if (df['period'].iloc[x] == half):
+        if half != 0:
+            
+            if (df['period'].iloc[x] == half):
+
+                if any([df['pass.outcome.name'].iloc[x] == "Incomplete",
+                        df['pass.outcome.name'].iloc[x] == "Unknown",
+                        df['pass.outcome.name'].iloc[x] == 'Injury Clearance',
+                        df['pass.outcome.name'].iloc[x] == 'Out',
+                        df['pass.outcome.name'].iloc[x] == 'Pass Offside']):
+                    
+                    bad_passes += 1
+                else:
+                    good_passes += 1
+        else:
 
             if any([df['pass.outcome.name'].iloc[x] == "Incomplete",
                     df['pass.outcome.name'].iloc[x] == "Unknown",
                     df['pass.outcome.name'].iloc[x] == 'Injury Clearance',
                     df['pass.outcome.name'].iloc[x] == 'Out',
                     df['pass.outcome.name'].iloc[x] == 'Pass Offside']):
-                
+                    
                 bad_passes += 1
             else:
                 good_passes += 1
+
 
     return round((good_passes / (good_passes + bad_passes)) * 100) # successful pass percentage
 
