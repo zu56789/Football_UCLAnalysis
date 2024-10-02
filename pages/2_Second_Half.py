1eabb7047db7ec03759d47c3e559738a9962aa1b
zu56789 <zuhayrmohsen@gmail.com>
2024-10-02 22:06:30 +0100

    Added expanders to show passing accuracy for second half

--- pages/2_Second_Half.py
+++ pages/2_Second_Half.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from data_extract import chels_passes_df, bayern_passes_df, chels_shots_df, bayern_shots_df
 from draw_pitches import passing_pitch, shooting_pitch
+from metric_analysis import get_passing_accuracy
 
 st.set_page_config(page_title="Second Half")
 
@@ -11,11 +12,15 @@ chels_tab, bayern_tab = st.tabs(["Chelsea", "Bayern Munich"])
 with chels_tab:
     st.write("Passing map")
     st.pyplot(passing_pitch(chels_passes_df,2))
+    expander = st.expander("Passing stats")
+    expander.write("Passing accuracy: " + str(get_passing_accuracy(chels_passes_df, 2)) + "%")
     st.write("Shooting map")
     st.pyplot(shooting_pitch(chels_shots_df,2))
 
 with bayern_tab:
     st.write("Passing map")
     st.pyplot(passing_pitch(bayern_passes_df,2))
+    expander = st.expander("Passing stats")
+    expander.write("Passing accuracy: " + str(get_passing_accuracy(bayern_passes_df, 2)) + "%")
     st.write("Shooting map")
     st.pyplot(shooting_pitch(bayern_shots_df,2))
