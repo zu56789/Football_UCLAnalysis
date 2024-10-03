e136430a9b82adaa88eaf340452ac4684fd380a8
zu56789 <zuhayrmohsen@gmail.com>
2024-10-03 02:42:34 +0100

    Added method for analysing shot accuracy

--- metric_analysis.py
+++ metric_analysis.py
@@ -48,3 +48,26 @@ def get_xg(df,half=0):
                 xg += df['shot.statsbomb_xg'].iloc[x]
 
     return round(xg,2)
+
+def get_shot_accuracy(df,half=0):
+    num_shots = 0
+    on_target = 0
+    off_target = 0
+
+    for x in range(len(df['id'])):
+
+        if half == 0:
+            if df['period'].iloc[x] != 5:
+                num_shots += 1
+                if df['shot.outcome.name'].iloc[x] == "Saved" or df['shot.outcome.name'].iloc[x] == "Goal":
+                    on_target += 1
+                else:
+                    off_target += 1
+        else:
+            if df['period'].iloc[x] == half and half != 5: # not including the penalty shootout
+                num_shots += 1
+                if df['shot.outcome.name'].iloc[x] == "Saved" or df['shot.outcome.name'].iloc[x] == "Goal":
+                    on_target += 1
+                else:
+                    off_target += 1
+    return round((on_target / num_shots) * 100), num_shots, on_target, off_target
