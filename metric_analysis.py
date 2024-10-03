18105315407b9c03224c3659dc7e57fce39abfc7
zu56789 <zuhayrmohsen@gmail.com>
2024-10-03 01:42:46 +0100

    Refactored get passing accuracy to also return total passes and completed passes

--- metric_analysis.py
+++ metric_analysis.py
@@ -1,12 +1,12 @@
 def get_passing_accuracy(df, half = 0):
 
     bad_passes = 0
-
     good_passes = 0
+    total_passes = 0
 
     for x in range(len(df['id'])):
         if half != 0:
-            
+
             if (df['period'].iloc[x] == half):
 
                 if any([df['pass.outcome.name'].iloc[x] == "Incomplete",
@@ -30,8 +30,9 @@ def get_passing_accuracy(df, half = 0):
             else:
                 good_passes += 1
 
+    total_passes = good_passes + bad_passes
 
-    return round((good_passes / (good_passes + bad_passes)) * 100) # successful pass percentage
+    return round((good_passes / (good_passes + bad_passes)) * 100), total_passes, good_passes
 
 
 def get_xg(df,half=0):
@@ -40,10 +41,10 @@ def get_xg(df,half=0):
          
         if half == 0:
             if df['period'].iloc[x] != 5:
-                xg += df['shot.statsbomb_xg'].iloc[x]
+                xg += df['shot.statsbomb_xg'].iloc[x] # not including penalty shootout
 
         else:
-            if df['period'].iloc[x] == half and half != 5: # not including penalty shootout
+            if df['period'].iloc[x] == half and half != 5: 
                 xg += df['shot.statsbomb_xg'].iloc[x]
 
     return round(xg,2)
