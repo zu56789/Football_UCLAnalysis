ea214aae7456f287dae0fa863fd6b4bb07169379
zu56789 <zuhayrmohsen@gmail.com>
2024-10-06 00:56:12 +0100

    Created method to draw bar charts

--- pages/4_Full_Time_Stats.py
+++ pages/4_Full_Time_Stats.py
@@ -4,22 +4,55 @@ import numpy as np
 from data_extract import chels_passes_df, bayern_passes_df, chels_shots_df, bayern_shots_df
 from metric_analysis import get_passing_accuracy, get_xg, get_shot_accuracy
 
-chels_passing_accuracy = get_passing_accuracy(chels_passes_df)[0]
-chels_total_passes = get_passing_accuracy(chels_passes_df)[1]
-chels_completed_passes = get_passing_accuracy(chels_passes_df)[2]
+chels_shooting = {
+    "Expected Goals": get_xg(chels_shots_df),
+    "Shooting Accuracy (%)": get_shot_accuracy(chels_shots_df)[0],
+    "Total Shots": get_shot_accuracy(chels_shots_df)[1],
+    "Shots on Target": get_shot_accuracy(chels_shots_df)[2],
+    "Shots off Target": get_shot_accuracy(chels_shots_df)[3]
+}
 
-chels_xg = get_xg(chels_shots_df)
-chels_shot_accuracy = get_shot_accuracy(chels_shots_df)[0]
-chels_num_shots = get_shot_accuracy(chels_shots_df)[1]
-chels_shots_on_target = get_shot_accuracy(chels_shots_df)[2]
-chels_shots_off_target = get_shot_accuracy(chels_shots_df)[3]
+chels_passing = {
+    "Passing Accuracy (%)": get_passing_accuracy(chels_passes_df)[0],
+    "Total Pass Attempts": get_passing_accuracy(chels_passes_df)[1],
+    "Completed Passes": get_passing_accuracy(chels_passes_df)[2]
+}
 
-bayern_passing_accuracy = get_passing_accuracy(bayern_passes_df)[0]
-bayern_total_passes = get_passing_accuracy(bayern_passes_df)[1]
-bayern_completed_passes = get_passing_accuracy(bayern_passes_df)[2]
+bayern_shooting = {
+    "Expected Goals": get_xg(bayern_shots_df),
+    "Shooting Accuracy (%)": get_shot_accuracy(bayern_shots_df)[0],
+    "Total Shots": get_shot_accuracy(bayern_shots_df)[1],
+    "Shots on Target": get_shot_accuracy(bayern_shots_df)[2],
+    "Shots off Target": get_shot_accuracy(bayern_shots_df)[3]
+}
 
-bayern_xg = get_xg(bayern_shots_df)
-bayern_shot_accuracy = get_shot_accuracy(bayern_shots_df)[0]
-bayern_num_shots = get_shot_accuracy(bayern_shots_df)[1]
-bayern_shots_on_target = get_shot_accuracy(bayern_shots_df)[2]
-bayern_shots_off_target = get_shot_accuracy(bayern_shots_df)[3]
+bayern_passing = {
+    "Passing Accuracy (%)": get_passing_accuracy(bayern_passes_df)[0],
+    "Total Pass Attempts": get_passing_accuracy(bayern_passes_df)[1],
+    "Completed Passes": get_passing_accuracy(bayern_passes_df)[2]
+}
+
+def draw_bar_charts(chels_stats, bayern_stats, labels, title):
+    chels_values = list(chels_stats.values())
+    bayern_values = list(bayern_stats.values())
+
+    bar_width = 0.3
+
+    fig, ax = plt.subplots(figsize=(12,7))
+
+    bars_chels = ax.barh(np.arange(len(labels)), chels_values, bar_width, label = "Chelsea", color= "blue")
+
+    bars_bayern = ax.barh(np.arange(len(labels)) + bar_width, bayern_values, bar_width, label = "Bayern", color= "red")
+
+    ax.set_xlabel('Values')
+    ax.set_title(title)
+    ax.set_yticks(np.arange(len(labels)) + bar_width / 2)
+    ax.set_yticklabels(labels)
+    ax.legend()
+
+    for bar in bars_chels:
+        ax.text(bar.get_width(), bar.get_y() + bar.get_height()/2, str(bar.get_width()), va='center')
+    for bar in bars_bayern:
+        ax.text(bar.get_width(), bar.get_y() + bar.get_height()/2, str(bar.get_width()), va='center')
+
+    return fig
